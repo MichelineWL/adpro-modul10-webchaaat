5eb44e596566f99354aca3b2da3880c698db82fd
MichelineWL <michelinewijaya@gmail.com>
2025-05-23 20:50:06 +0700

    “Experiment 3.1: Original code

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,8 @@
-import('./pkg').then((module) => {
-    module.run_app();
-});
+import init from "./pkg/yewchat.js";
+
+async function main() {
+  const wasm = await init();  // init() mengembalikan InitOutput yang punya run_app()
+  wasm.run_app();             // baru panggil run_app() dari objek itu
+}
+
+main();
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,31 @@
 #![recursion_limit = "512"]
 
 use wasm_bindgen::prelude::*;
-use yew::functional::*;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// Your main component
+struct Model;
 
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <div>{ "Hello from Yew!" }</div>
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() {
+    yew::start_app::<Model>();
+}
